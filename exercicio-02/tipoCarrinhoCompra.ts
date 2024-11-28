93b0a42b3e26af3b7c1f8ac772cc53d9849f6ff0
hercilio17t <136920405+hercilio17t@users.noreply.github.com>
2024-11-27 22:10:31 -0300

    Exercício 2

--- exercicio-02/tipoCarrinhoCompra.ts
+++ exercicio-02/tipoCarrinhoCompra.ts
@@ -1,3 +1,21 @@
+/*
+Problema
+No arquivo tipoCarrinhoCompra.ts, dentro da pasta referente à essa questão, 
+foi implementado um tipo para ser usado nas funções que fazem a gestão do carrinho de compras de um site. 
+Porém, a estrutura do tipo não tinha o endereço de entrega, e a propriedade tipoTransacao, que tem um tipo literal, 
+precisa ser padronizada para que aceite apenas caracteres minúsculos.
+
+Como precisamos manter a integridade do projeto e o tipo já foi usado em outras funções no projeto, 
+implemente um novo tipo, partindo do atual, e adicione uma propriedade endereco, que receberá um objeto com o formato abaixo. 
+Manipule o tipo da propriedade tipoTransacao para aceitar apenas caracteres minúsculos.
+
+cep
+rua
+bairro
+cidade
+estado
+*/
+
 type Item = {
     nome: string,
     descricao: string,
@@ -18,4 +36,17 @@ type Carrinho = {
     frete: number,
     tipoTransacao: 'credito' | 'Debito',
     cartao: Cartao
+}
+
+type Endereco = {
+    cep: string,
+    rua: string,
+    bairro: string,
+    cidade: string,
+    estado: string
+}
+
+type NovoCarrinho = Omit<Carrinho, 'tipoTransacao'> & {
+    tipoTransacao: 'credito' | 'debito',
+    endereco: Endereco
 }
